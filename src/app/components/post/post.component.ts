e736e96b0c8abcd4b4a68d0283541efd1fee0f66
pbravi <52786684+pbravi@users.noreply.github.com>
2019-10-10 02:27:52 -0300

    Update post.component.ts

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -4,8 +4,7 @@ import{ PostService, Post} from '../../services/post.service';
 
 @Component({
   selector: 'app-post',
-  templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  templateUrl: './post.component.html'
 })
 export class PostComponent implements OnInit {
 
